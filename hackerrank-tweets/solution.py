6be9586cf00e7ad5ee9b9c5c37018d7dae7a2b03
Salvador  Tamarit Muñoz <tama@coltrane.local>
2022-07-03 17:45:33 +0200

    hackerrank-tweets https://www.hackerrank.com/challenges/hackerrank-tweets/

---
+++ hackerrank-tweets/solution.py
@@ -0,0 +1,14 @@
+# https://www.hackerrank.com/challenges/detecting-valid-latitude-and-longitude
+
+import re
+    
+if __name__ == '__main__':
+    total = int(input())
+    sentences = []
+    for _ in range(total):
+        sentences.append(input().strip())
+    count = 0
+    for s in sentences:
+        if re.search(r"[\#\@]?[Hh][Aa][cC][kK][eE][rR][rR][aA][nN][kK]", s):
+            count += 1
+    print(count)
